520eca5b6dd57ca6c4e0c7363f4859af54269c40
Pavel Osminin <pav.osminin@gmail.com>
2019-03-21 21:51:27 +0500

    initial version

---
+++ 14_Tree/Tests.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsDataStructures2
+{
+    class Program
+    {
+        
+        static void PrintWithLevel(SimpleTree<int> Tree)
+        {
+            Queue<SimpleTreeNode<int>> numbers = new Queue<SimpleTreeNode<int>>();
+            Queue<SimpleTreeNode<int>> neighbors = new Queue<SimpleTreeNode<int>>();
+            if (Tree.Root.Children != null)
+            {
+                numbers.Enqueue(Tree.Root);
+                int level = 0;
+
+                while (numbers.Count != 0 || neighbors.Count != 0)
+                {
+                    Console.WriteLine("Node level: {0}", level);
+                    while (numbers.Count != 0)
+                    {
+                        SimpleTreeNode<int> tempNode = numbers.Dequeue();
+                        Console.WriteLine(tempNode.NodeValue);
+                        if (tempNode.Children != null && tempNode.Children.Count > 0)
+                        {
+
+                            for (int i = 0; i < tempNode.Children.Count; i++)
+                                neighbors.Enqueue(tempNode.Children[i]);
+                        }
+                    }
+                    Console.WriteLine();
+                    level++;
+                    Console.WriteLine("Node level: {0}", level);
+                    while (neighbors.Count != 0)
+                    {
+                        SimpleTreeNode<int> tempNode2 = neighbors.Dequeue();
+                        Console.WriteLine(tempNode2.NodeValue);
+                        if (tempNode2.Children != null && tempNode2.Children.Count > 0)
+                        {
+
+                            for (int i = 0; i < tempNode2.Children.Count; i++)
+                                numbers.Enqueue(tempNode2.Children[i]);
+                        }
+                    }
+                    Console.WriteLine();
+                    level++;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Tree is empty");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+           
+            // define test nodes
+            SimpleTreeNode<int> root9 = new SimpleTreeNode<int>(9, null);
+            SimpleTreeNode<int> node4 = new SimpleTreeNode<int>(4, root9);
+            SimpleTreeNode<int> node17 = new SimpleTreeNode<int>(17, root9);
+            SimpleTreeNode<int> node3 = new SimpleTreeNode<int>(3, node4);
+            SimpleTreeNode<int> node6 = new SimpleTreeNode<int>(6, node4);
+            SimpleTreeNode<int> node22 = new SimpleTreeNode<int>(22, node17);
+            SimpleTreeNode<int> node5 = new SimpleTreeNode<int>(5, node6);
+            SimpleTreeNode<int> node7 = new SimpleTreeNode<int>(7, node6);
+            SimpleTreeNode<int> node20 = new SimpleTreeNode<int>(20, node22);
+            SimpleTreeNode<int> node99 = new SimpleTreeNode<int>(99, node7);
+            // define test tree
+            SimpleTree<int> Tree = new SimpleTree<int>(root9);
+            Tree.AddChild(root9, node4);
+            Console.WriteLine("AddChild method test, adding a new node");
+            Tree.AddChild(root9, node17);
+            if (root9.Children[1].NodeValue==17 && root9.Children[1].Parent==root9)
+            {
+                Console.WriteLine("OK");
+            }
+            else
+            {
+                Console.WriteLine("FAIL");
+            }
+            Tree.AddChild(node4, node3);
+            Tree.AddChild(node4, node6);
+            Tree.AddChild(node17, node22);
+            Tree.AddChild(node22, node20);
+            Tree.AddChild(node6, node5);
+            Tree.AddChild(node6, node7);
+            Tree.AddChild(node7, node99);
+
+            Console.WriteLine("DeleteNode method test, deleting a new node");
+            Tree.DeleteNode(node99);
+            if (node7.Children.Count == 0)
+            {
+                Console.WriteLine("OK");
+            }
+            else
+            {
+                Console.WriteLine("FAIL");
+            }
+
+            Console.WriteLine("GetAllNodes method test, getting all nodes");
+            List<SimpleTreeNode<int>> returnedNodes = new List<SimpleTreeNode<int>>();
+            returnedNodes=Tree.GetAllNodes();
+            if (returnedNodes.Count == 9 && returnedNodes[0].NodeValue==9 && returnedNodes[1].NodeValue == 4
+                && returnedNodes[2].NodeValue == 17 && returnedNodes[3].NodeValue == 3 && returnedNodes[4].NodeValue == 6
+                && returnedNodes[5].NodeValue == 22 && returnedNodes[6].NodeValue == 5 && returnedNodes[7].NodeValue == 7
+                && returnedNodes[8].NodeValue == 20)
+                
+            {
+                Console.WriteLine("OK");
+            }
+            else
+            {
+                Console.WriteLine("FAIL");
+            }
+
+            Console.WriteLine("FindNodesByValue method test, finding a node by value");
+            List<SimpleTreeNode<int>> returnedNode = new List<SimpleTreeNode<int>>();
+            returnedNode=Tree.FindNodesByValue(6);
+            if (returnedNode.Count == 1 && returnedNode[0].NodeValue == 6)
+            {
+                Console.WriteLine("OK");
+            }
+            else
+            {
+                Console.WriteLine("FAIL");
+            }
+            Console.WriteLine("MoveNode method test, moving a node with its subtree");
+            Tree.MoveNode(node6, root9);
+            if (Tree.Root.Children[0].Children.Count == 1 && Tree.Root.Children.Count == 3
+                && Tree.Root.Children[2].Children.Count == 2 && Tree.Root.Children[2].NodeValue == 6)
+            {
+                Console.WriteLine("OK");
+            }
+            else
+            {
+                Console.WriteLine("FAIL");
+            }
+            Console.WriteLine("Count nodes method test, counting nodes");
+            if (Tree.Count() == 9)
+            {
+                Console.WriteLine("OK");
+            }
+            else
+            {
+                Console.WriteLine("FAIL");
+            }
+            Console.WriteLine("LeafCount method test, counting leaves");
+            if (Tree.LeafCount() == 4)
+            {
+                Console.WriteLine("OK");
+            }
+            else
+            {
+                Console.WriteLine("FAIL");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Printing tree by levels");
+            PrintWithLevel(Tree);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ 14_Tree/Tree.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsDataStructures2
+{
+    public class SimpleTreeNode<T>
+    {
+        public T NodeValue; // значение в узле
+        public SimpleTreeNode<T> Parent; // родитель или null для корня
+        public List<SimpleTreeNode<T>> Children; // список дочерних узлов или null
+
+        public SimpleTreeNode(T val, SimpleTreeNode<T> parent)
+        {
+            NodeValue = val;
+            Parent = parent;
+            Children = null;
+        }
+    }
+
+    public class SimpleTree<T>
+    {
+        public SimpleTreeNode<T> Root; // корень, может быть null
+
+        public SimpleTree(SimpleTreeNode<T> root)
+        {
+            Root = root;
+        }
+
+        public void AddChild(SimpleTreeNode<T> ParentNode, SimpleTreeNode<T> NewChild)
+        {
+            // ваш код добавления нового дочернего узла существующему ParentNode
+            if (ParentNode.Children == null)
+            {
+                List<SimpleTreeNode<T>> Children = new List<SimpleTreeNode<T>>();
+                ParentNode.Children = Children;
+                ParentNode.Children.Add(NewChild);
+                NewChild.Parent = ParentNode;
+            }
+            else
+            {
+                if (!ParentNode.Children.Contains(NewChild) && ParentNode != NewChild)
+                {
+                    ParentNode.Children.Add(NewChild);
+                    NewChild.Parent = ParentNode;
+                }
+            }
+        }
+
+        public void DeleteNode(SimpleTreeNode<T> NodeToDelete)
+        {
+            // ваш код удаления существующего узла NodeToDelete
+            NodeToDelete.Parent.Children.Remove(NodeToDelete);
+            NodeToDelete.Parent = null;
+            
+        }
+
+        public List<SimpleTreeNode<T>> GetAllNodes()
+        {
+            // ваш код выдачи всех узлов дерева в определённом порядке
+            List<SimpleTreeNode<T>> nodes = new List<SimpleTreeNode<T>>();
+            Queue<SimpleTreeNode<T>> numbers = new Queue<SimpleTreeNode<T>>();
+            if (Root.Children != null)
+            {
+                numbers.Enqueue(Root);
+                while (numbers.Count != 0)
+                {
+                    SimpleTreeNode<T> tempNode = numbers.Dequeue();
+                   
+                        nodes.Add(tempNode);
+        
+                    if (tempNode.Children != null && tempNode.Children.Count > 0)
+                    {
+                        for (int i = 0; i < tempNode.Children.Count; i++)
+                            numbers.Enqueue(tempNode.Children[i]);
+                    }
+                }
+            }
+            return nodes;
+        }
+
+        public List<SimpleTreeNode<T>> FindNodesByValue(T val)
+        {
+            // ваш код поиска узлов по значению
+            List<SimpleTreeNode<T>> nodes = new List<SimpleTreeNode<T>>();
+            Queue<SimpleTreeNode<T>> numbers = new Queue<SimpleTreeNode<T>>();
+            if (Root.Children != null)
+            {
+                numbers.Enqueue(Root);
+                while (numbers.Count != 0)
+                {
+                    SimpleTreeNode<T> tempNode = numbers.Dequeue();
+                    if (Equals(tempNode.NodeValue, val))
+                    {
+                        nodes.Add(tempNode);
+                    }
+                    if (tempNode.Children!=null && tempNode.Children.Count>0)
+                    {
+                        for (int i = 0; i< tempNode.Children.Count; i++)
+                        numbers.Enqueue(tempNode.Children[i]);
+                    }
+                }
+            }
+            return nodes;
+        }
+
+        public void MoveNode(SimpleTreeNode<T> OriginalNode, SimpleTreeNode<T> NewParent)
+        {
+            // ваш код перемещения узла вместе с его поддеревом -- 
+            // в качестве дочернего для узла NewParent
+            if (OriginalNode.Children==null || OriginalNode.Children.Count==0)
+            {
+                SimpleTreeNode<T> tempNode = OriginalNode;
+                DeleteNode(OriginalNode);
+                AddChild(NewParent, tempNode);
+            }
+            else
+            {
+                SimpleTreeNode<T> tempNode = OriginalNode;
+                DeleteNode(OriginalNode);
+                AddChild(NewParent, tempNode);
+                for (int i = 0; i < tempNode.Children.Count; i++)
+                {
+                    SimpleTreeNode<T> tempNode2 = tempNode.Children[i];
+                    AddChild(tempNode, tempNode2);
+                }
+            }
+        }
+        
+
+        public int Count()
+        {
+            // количество всех узлов в дереве
+            int nodesNubmer = 0;
+            Queue<SimpleTreeNode<T>> numbers = new Queue<SimpleTreeNode<T>>();
+            if (Root.Children != null)
+            {
+                numbers.Enqueue(Root);
+
+                while (numbers.Count != 0)
+                {
+                    SimpleTreeNode<T> tempNode = numbers.Dequeue();
+                    nodesNubmer++;
+                    if (tempNode.Children != null && tempNode.Children.Count > 0)
+                    {
+                        for (int i = 0; i < tempNode.Children.Count; i++)
+                            numbers.Enqueue(tempNode.Children[i]);
+                    }
+                }
+            }
+            return nodesNubmer;
+        }
+
+        public int LeafCount()
+        {
+            // количество листьев в дереве
+            int leavesNubmer = 0;
+            Queue<SimpleTreeNode<T>> numbers = new Queue<SimpleTreeNode<T>>();
+            if (Root.Children != null)
+            {
+                numbers.Enqueue(Root);
+                while (numbers.Count != 0)
+                {
+                    SimpleTreeNode<T> tempNode = numbers.Dequeue();
+
+                    if (tempNode.Children == null || tempNode.Children.Count == 0) leavesNubmer++;
+
+                    if (tempNode.Children != null && tempNode.Children.Count > 0)
+                    {
+                        for (int i = 0; i < tempNode.Children.Count; i++)
+                            numbers.Enqueue(tempNode.Children[i]);
+                    }
+                }
+            }
+            return leavesNubmer;
+        }
+
+    }
+
+}
