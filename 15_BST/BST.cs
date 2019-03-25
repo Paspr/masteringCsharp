2867e7056bdda01ba853efd1c44ed7c9e563b668
Pavel Osminin <pav.osminin@gmail.com>
2019-03-25 19:16:50 +0500

    initial version

---
+++ 15_BST/BST.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsDataStructures2
+{
+    public class BSTNode<T>
+    {
+        public int NodeKey; // ключ узла
+        public T NodeValue; // значение в узле
+        public BSTNode<T> Parent; // родитель или null для корня
+        public BSTNode<T> LeftChild; // левый потомок
+        public BSTNode<T> RightChild; // правый потомок	
+
+        public BSTNode(int key, T val, BSTNode<T> parent)
+        {
+            NodeKey = key;
+            NodeValue = val;
+            Parent = parent;
+            LeftChild = null;
+            RightChild = null;
+        }
+    }
+
+    // промежуточный результат поиска
+    public class BSTFind<T>
+    {
+        // null если не найден узел, и в дереве только один корень
+        public BSTNode<T> Node;
+
+        // true если узел найден
+        public bool NodeHasKey;
+
+        // true, если родительскому узлу надо добавить новый левым
+        public bool ToLeft;
+
+        public BSTFind() { Node = null; }
+    }
+
+    public class BST<T>
+    {
+        public BSTNode<T> Root; // корень дерева, или null
+
+        public BST(BSTNode<T> node)
+        {
+            Root = node;
+        }
+
+        public BSTFind<T> FindNodeByKey(int key)
+        {
+            BSTFind<T> result = new BSTFind<T>();
+            // ищем в дереве узел и сопутствующую информацию по ключу
+            Queue<BSTNode<T>> nodes = new Queue<BSTNode<T>>();
+            if (Root != null)
+            {
+                nodes.Enqueue(Root);
+                while (nodes.Count != 0)
+                {
+                    BSTNode<T> tempNode = nodes.Dequeue();
+                    if (tempNode.NodeKey == key)
+                    {
+
+                        result.NodeHasKey = true;
+                        result.Node = tempNode;
+                        return result;
+                    }
+                    else if (tempNode.NodeKey < key)
+                    {
+                        if (tempNode.RightChild == null)
+                        {
+                            result.NodeHasKey = false;
+                            result.Node = tempNode;
+                            result.ToLeft = false;
+                            return result;
+                        }
+                        else
+                        {
+                            if (tempNode.RightChild.NodeKey == key)
+                            {
+
+                                result.NodeHasKey = true;
+                                result.Node = tempNode.RightChild;
+                                return result;
+                            }
+                            else
+                            {
+                                nodes.Enqueue(tempNode.RightChild);
+                            }
+                        }
+                    }
+                    else if (tempNode.NodeKey > key)
+                    {
+                        if (tempNode.LeftChild == null)
+                        {
+                            result.NodeHasKey = false;
+                            result.Node = tempNode;
+                            result.ToLeft = true;
+                            return result;
+                        }
+                        else
+                        {
+                            if (tempNode.LeftChild.NodeKey == key)
+                            {
+
+                                result.NodeHasKey = true;
+                                result.Node = tempNode.LeftChild;
+                                return result;
+                            }
+                            else
+                            {
+                                nodes.Enqueue(tempNode.LeftChild);
+                            }
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+        
+
+        public bool AddKeyValue(int key, T val)
+        {
+            // добавляем ключ-значение в дерево
+            BSTFind<T> result = new BSTFind<T>();
+            result = FindNodeByKey(key);
+            if (result == null || result.NodeHasKey==true)
+            {
+                return false; // если ключ уже есть
+            }
+            else
+            {
+                if (result.ToLeft==true)
+                {
+                    BSTNode<T> parentNode = result.Node;
+                    BSTNode<T> addedNode = new BSTNode<T>(key, val, parentNode);
+                    parentNode.LeftChild = addedNode;
+                    return true;
+                }
+                if (result.ToLeft == false)
+                {
+                    BSTNode<T> parentNode = result.Node;
+                    BSTNode<T> addedNode = new BSTNode<T>(key, val, parentNode);
+                    parentNode.RightChild = addedNode;
+                    return true;
+                }
+            }
+            return true;
+
+
+        }
+
+        public BSTNode<T> FinMinMax(BSTNode<T> FromNode, bool FindMax)
+        {
+            // ищем максимальное/минимальное в поддереве
+            Queue<BSTNode<T>> nodes = new Queue<BSTNode<T>>();
+            if (FromNode != null)
+            {
+                nodes.Enqueue(FromNode);
+                while (nodes.Count != 0)
+                {
+                    BSTNode<T> tempNode = nodes.Dequeue();
+                    if (FindMax==true)
+                    {
+                        if (tempNode.RightChild == null) return tempNode;
+                        else
+                        {
+                            nodes.Enqueue(tempNode.RightChild);
+                        }
+                    }
+                    if (FindMax == false)
+                    {
+                        if (tempNode.LeftChild == null) return tempNode;
+                        else
+                        {
+                            nodes.Enqueue(tempNode.LeftChild);
+                        }
+                    }
+                }
+                return nodes.Dequeue();
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public bool DeleteNodeByKey(int key)
+        {
+            // удаляем узел по ключу
+            BSTFind<T> result = new BSTFind<T>();
+            result = FindNodeByKey(key);
+            if (result.NodeHasKey == false) return false; // если узел не найден
+            else
+            {
+                if (result.Node.LeftChild == null && result.Node.RightChild == null)
+                {
+                    if (result.Node.Parent.LeftChild.NodeKey == key) result.Node.Parent.LeftChild = null;
+                    if (result.Node.Parent.RightChild.NodeKey == key) result.Node.Parent.RightChild = null;
+                }
+                if (result.Node.LeftChild == null && result.Node.RightChild != null)
+                {
+                    if (result.Node.Parent.RightChild.NodeKey == key)
+                    {
+                        BSTNode<T> tempNode = result.Node.RightChild;
+                        result.Node.Parent.RightChild.NodeKey = tempNode.NodeKey;
+                        result.Node.Parent.RightChild.NodeValue = tempNode.NodeValue;
+                        result.Node.Parent.RightChild.RightChild = null;
+                    }
+                }
+                if (result.Node.LeftChild != null && result.Node.RightChild == null)
+                {
+                    if (result.Node.Parent.LeftChild.NodeKey == key)
+                    {
+                        BSTNode<T> tempNode = result.Node.LeftChild;
+                        result.Node.NodeKey = tempNode.NodeKey; // result.Node.Parent.LeftChild.NodeKey = tempNode.NodeKey;
+                        result.Node.NodeValue = tempNode.NodeValue; // result.Node.Parent.LeftChild.NodeValue = tempNode.NodeValue;
+                        result.Node.RightChild = null; //result.Node.Parent.LeftChild.LeftChild
+                    }
+                }
+                if (result.Node.LeftChild != null && result.Node.RightChild != null)
+                {
+                    Queue<BSTNode<T>> nodes = new Queue<BSTNode<T>>();
+                    nodes.Enqueue(result.Node.RightChild);
+                    while (nodes.Count != 0)
+                    {
+                        BSTNode<T> tempNode = nodes.Dequeue();
+                        if (tempNode.LeftChild == null && tempNode.RightChild == null)
+                        {
+                            result.Node.NodeKey = tempNode.NodeKey; // result.Node.Parent.LeftChild.NodeValue = tempNode.NodeValue
+                            result.Node.NodeValue = tempNode.NodeValue; // result.Node.Parent.LeftChild.NodeKey = tempNode.NodeKey
+                            tempNode.Parent.LeftChild = null; // result.Node.Parent.LeftChild.RightChild.LeftChild
+                            break;
+                        }
+                        if (tempNode.LeftChild == null && tempNode.RightChild != null)
+                        {
+                            result.Node.NodeKey = tempNode.RightChild.NodeKey;
+                            result.Node.NodeValue = tempNode.RightChild.NodeValue;
+                            result.Node.RightChild.RightChild = null;
+                            break;
+                        }
+                        else
+                        {
+                            nodes.Enqueue(tempNode.LeftChild);
+                        }
+                    }
+                }
+                return true;
+            }
+        }
+
+    }
+}
---
+++ 15_BST/Tests.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsDataStructures2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // define test nodes
+            BSTNode<int> root8 = new BSTNode<int>(8, 8, null);
+
+            // define test tree
+            BST<int> BinTree = new BST<int>(root8);
+            
+            // search test
+            Console.WriteLine("FindNodeByKey method test");
+            Console.WriteLine("search for existing key");
+            if (BinTree.FindNodeByKey(8).NodeHasKey==true && BinTree.FindNodeByKey(8).Node.NodeKey==8)
+            {
+                Console.WriteLine("OK");
+            }
+            else
+            {
+                Console.WriteLine("FAIL");
+            }
+            Console.WriteLine("requested key will be added as a left child");
+            if (BinTree.FindNodeByKey(4).NodeHasKey == false && BinTree.FindNodeByKey(4).ToLeft == true)
+            {
+                Console.WriteLine("OK");
+            }
+            else
+            {
+                Console.WriteLine("FAIL");
+            }
+            Console.WriteLine("requested key will be added as a right child");
+            if (BinTree.FindNodeByKey(12).NodeHasKey == false && BinTree.FindNodeByKey(12).ToLeft == false)
+            {
+                Console.WriteLine("OK");
+            }
+            else
+            {
+                Console.WriteLine("FAIL");
+            }
+            // adding test
+            Console.WriteLine();
+            Console.WriteLine("AddKeyValue method test");
+            Console.WriteLine("adding node as a left child");
+            if (BinTree.FindNodeByKey(4).NodeHasKey == false && BinTree.AddKeyValue(4, 4))
+            {
+                if (BinTree.FindNodeByKey(4).NodeHasKey == true) Console.WriteLine("OK");
+
+            }
+            else
+            {
+                Console.WriteLine("FAIL");
+            }
+            Console.WriteLine("adding node as a right child");
+            if (BinTree.FindNodeByKey(12).NodeHasKey == false && BinTree.AddKeyValue(12, 12))
+            {
+                if (BinTree.FindNodeByKey(12).NodeHasKey == true) Console.WriteLine("OK");
+
+            }
+            else
+            {
+                Console.WriteLine("FAIL");
+            }
+            Console.WriteLine("adding existing node");
+            if (BinTree.AddKeyValue(12, 12)==false)
+            {
+                Console.WriteLine("OK");
+
+            }
+            else
+            {
+                Console.WriteLine("FAIL");
+            }
+            // find max and min key test
+            BinTree.AddKeyValue(2, 2);
+            BinTree.AddKeyValue(1, 1);
+            BinTree.AddKeyValue(3, 3);
+            BinTree.AddKeyValue(6, 6);
+            BinTree.AddKeyValue(5, 5);
+            BinTree.AddKeyValue(7, 7);
+            BinTree.AddKeyValue(10, 10);
+            BinTree.AddKeyValue(9, 9);
+            BinTree.AddKeyValue(11, 11);
+            BinTree.AddKeyValue(14, 14);
+            BinTree.AddKeyValue(13, 13);
+            BinTree.AddKeyValue(15, 15);
+            Console.WriteLine();
+            Console.WriteLine("FinMinMax method test");
+            Console.WriteLine("Search max key value starting from the root");
+            if (BinTree.FinMinMax(BinTree.Root, true).NodeKey==15)
+            {
+                Console.WriteLine("OK");
+
+            }
+            else
+            {
+                Console.WriteLine("FAIL");
+            }
+            Console.WriteLine("Search max key value starting from the subtree");
+            if (BinTree.FinMinMax(BinTree.Root.LeftChild, true).NodeKey == 7)
+            {
+                Console.WriteLine("OK");
+
+            }
+            else
+            {
+                Console.WriteLine("FAIL");
+            }
+            Console.WriteLine("Search min key value starting from the root");
+            if (BinTree.FinMinMax(BinTree.Root, false).NodeKey == 1)
+            {
+                Console.WriteLine("OK");
+
+            }
+            else
+            {
+                Console.WriteLine("FAIL");
+            }
+            Console.WriteLine("Search min key value starting from the subtree");
+            if (BinTree.FinMinMax(BinTree.Root.RightChild, false).NodeKey == 9)
+            {
+                Console.WriteLine("OK");
+
+            }
+            else
+            {
+                Console.WriteLine("FAIL");
+            }
+            // delete node test
+            Console.WriteLine();
+            Console.WriteLine("DeleteNodeByKey method test");
+            if (BinTree.FindNodeByKey(5).NodeHasKey == true)
+            {
+                if (BinTree.DeleteNodeByKey(5) && BinTree.Root.LeftChild.RightChild.LeftChild==null) Console.WriteLine("OK");
+
+            }
+            else
+            {
+                Console.WriteLine("FAIL");
+            }
+            if (BinTree.FindNodeByKey(4).NodeHasKey == true)
+            {
+                if (BinTree.DeleteNodeByKey(4) && BinTree.FindNodeByKey(4).NodeHasKey == false && BinTree.Root.LeftChild.NodeKey==7) Console.WriteLine("OK");
+
+            }
+            else
+            {
+                Console.WriteLine("FAIL");
+            }
+
+            BinTree.DeleteNodeByKey(12); 
+            BinTree.DeleteNodeByKey(13);
+            Console.ReadKey();
+        }
+    }
+}
