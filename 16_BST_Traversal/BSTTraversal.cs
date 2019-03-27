740edce77e8992fcb9563c71cbefe110b8734819
Pavel Osminin <pav.osminin@gmail.com>
2019-03-27 16:35:48 +0500

    initial version

---
+++ 16_BST_Traversal/BSTTraversal.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsDataStructures2
+{
+    public class BSTNode<T>
+    {
+        public int NodeKey; // ключ узла
+        public T NodeValue; // значение в узле
+        public BSTNode<T> Parent; // родитель или null для корня
+        public BSTNode<T> LeftChild; // левый потомок
+        public BSTNode<T> RightChild; // правый потомок	
+
+        public BSTNode(int key, T val, BSTNode<T> parent)
+        {
+            NodeKey = key;
+            NodeValue = val;
+            Parent = parent;
+            LeftChild = null;
+            RightChild = null;
+        }
+    }
+
+    // промежуточный результат поиска
+    public class BSTFind<T>
+    {
+        // null если не найден узел, и в дереве только один корень
+        public BSTNode<T> Node;
+
+        // true если узел найден
+        public bool NodeHasKey;
+
+        // true, если родительскому узлу надо добавить новый левым
+        public bool ToLeft;
+
+        public BSTFind() { Node = null; }
+    }
+
+    public class BST<T>
+    {
+        public BSTNode<T> Root; // корень дерева, или null
+
+        public BST(BSTNode<T> node)
+        {
+            Root = node;
+        }
+
+        public BSTFind<T> FindNodeByKey(int key)
+        {
+            BSTFind<T> result = new BSTFind<T>();
+            // ищем в дереве узел и сопутствующую информацию по ключу
+            Queue<BSTNode<T>> nodes = new Queue<BSTNode<T>>();
+            if (Root != null)
+            {
+                nodes.Enqueue(Root);
+                while (nodes.Count != 0)
+                {
+                    BSTNode<T> tempNode = nodes.Dequeue();
+                    if (tempNode.NodeKey == key)
+                    {
+
+                        result.NodeHasKey = true;
+                        result.Node = tempNode;
+                        return result;
+                    }
+                    else if (tempNode.NodeKey < key)
+                    {
+                        if (tempNode.RightChild == null)
+                        {
+                            result.NodeHasKey = false;
+                            result.Node = tempNode;
+                            result.ToLeft = false;
+                            return result;
+                        }
+                        else
+                        {
+                            if (tempNode.RightChild.NodeKey == key)
+                            {
+
+                                result.NodeHasKey = true;
+                                result.Node = tempNode.RightChild;
+                                return result;
+                            }
+                            else
+                            {
+                                nodes.Enqueue(tempNode.RightChild);
+                            }
+                        }
+                    }
+                    else if (tempNode.NodeKey > key)
+                    {
+                        if (tempNode.LeftChild == null)
+                        {
+                            result.NodeHasKey = false;
+                            result.Node = tempNode;
+                            result.ToLeft = true;
+                            return result;
+                        }
+                        else
+                        {
+                            if (tempNode.LeftChild.NodeKey == key)
+                            {
+
+                                result.NodeHasKey = true;
+                                result.Node = tempNode.LeftChild;
+                                return result;
+                            }
+                            else
+                            {
+                                nodes.Enqueue(tempNode.LeftChild);
+                            }
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+
+
+        public bool AddKeyValue(int key, T val)
+        {
+            // добавляем ключ-значение в дерево
+            BSTFind<T> result = new BSTFind<T>();
+            result = FindNodeByKey(key);
+            if (result == null || result.NodeHasKey == true)
+            {
+                return false; // если ключ уже есть
+            }
+            else
+            {
+                if (result.ToLeft == true)
+                {
+                    BSTNode<T> parentNode = result.Node;
+                    BSTNode<T> addedNode = new BSTNode<T>(key, val, parentNode);
+                    parentNode.LeftChild = addedNode;
+                    return true;
+                }
+                if (result.ToLeft == false)
+                {
+                    BSTNode<T> parentNode = result.Node;
+                    BSTNode<T> addedNode = new BSTNode<T>(key, val, parentNode);
+                    parentNode.RightChild = addedNode;
+                    return true;
+                }
+            }
+            return true;
+        }
+
+        public List<BSTNode<T>> WideAllNodes()
+        {
+            //Breadth - first traversal
+            Queue<BSTNode<T>> nodes = new Queue<BSTNode<T>>();
+            List<BSTNode<T>> result = new List<BSTNode<T>>();
+            if (Root != null)
+            {
+                nodes.Enqueue(Root);
+                while (nodes.Count != 0)
+                {
+                    BSTNode<T> tempNode = nodes.Dequeue();
+                    result.Add(tempNode);
+                    if (tempNode.LeftChild != null)
+                    {
+                        nodes.Enqueue(tempNode.LeftChild);
+                    }
+                    if (tempNode.RightChild != null)
+                    {
+                        nodes.Enqueue(tempNode.RightChild);
+                    }
+                }
+                return result;
+            }
+            else
+            {
+                return result;
+            }
+        }
+
+        public List<BSTNode<T>> DeepAllNodes(int traversalType) //  0 (in-order), 1 (post-order) и 2 (pre-order)
+        {
+            if (traversalType == 0) // 0 (in-order)
+            {
+                if (Root!=null)
+                { 
+                    Stack<BSTNode<T>> nodes = new Stack<BSTNode<T>>();
+                    List<BSTNode<T>> getnodes = new List<BSTNode<T>>();
+                    BSTNode<T> current = Root;
+                    while (current != null || nodes.Count != 0)
+                    {
+
+                        while (current != null)
+                        {
+                            nodes.Push(current);
+                            current = current.LeftChild;
+                        }
+                        current = nodes.Pop();
+                        getnodes.Add(current);
+                        current = current.RightChild;
+                    }
+                    return getnodes;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            else if (traversalType==1) // 1 (post-order)
+            {
+                if (Root != null)
+                {
+                    Stack<BSTNode<T>> nodes = new Stack<BSTNode<T>>();
+                    Stack<BSTNode<T>> nodesPostOrder = new Stack<BSTNode<T>>();
+                    List<BSTNode<T>> getnodes = new List<BSTNode<T>>();
+                    BSTNode<T> current = Root;
+                    nodes.Push(current);
+                    while (nodes.Count != 0)
+                    {
+                        current = nodes.Pop();
+                        nodesPostOrder.Push(current);
+                        if (current.LeftChild!=null) nodes.Push(current.LeftChild);
+                        if (current.RightChild != null) nodes.Push(current.RightChild);
+                        getnodes.Add(nodesPostOrder.Peek());
+                    }
+                    getnodes.Reverse();
+                    return getnodes;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            else if (traversalType==2) // 2 (pre-order)
+            {
+                if (Root != null)
+                {
+                    Stack<BSTNode<T>> nodes = new Stack<BSTNode<T>>();
+                    List<BSTNode<T>> getnodes = new List<BSTNode<T>>();
+                    BSTNode<T> current = Root;
+                    nodes.Push(current);
+                    while (nodes.Count!=0)
+                    {
+                        current = nodes.Pop();
+                        getnodes.Add(current);
+                        if (current.RightChild != null) nodes.Push(current.RightChild);
+                        if (current.LeftChild != null) nodes.Push(current.LeftChild);
+                    }
+                    return getnodes;
+                }
+                else
+                {
+                    return null;
+                }
+            } 
+            return null;
+        }
+
+    }
+}
---
+++ 16_BST_Traversal/Tests.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsDataStructures2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // define test nodes
+            BSTNode<int> root8 = new BSTNode<int>(8, 8, null);
+
+            // define test tree
+            BST<int> BinTree = new BST<int>(root8);
+            BinTree.AddKeyValue(4, 4);
+            BinTree.AddKeyValue(12, 12);
+            BinTree.AddKeyValue(2, 2);
+            BinTree.AddKeyValue(1, 1);
+            BinTree.AddKeyValue(3, 3);
+            BinTree.AddKeyValue(6, 6);
+            BinTree.AddKeyValue(5, 5);
+            BinTree.AddKeyValue(7, 7);
+            BinTree.AddKeyValue(10, 10);
+            BinTree.AddKeyValue(9, 9);
+            BinTree.AddKeyValue(11, 11);
+            BinTree.AddKeyValue(14, 14);
+            BinTree.AddKeyValue(13, 13);
+            BinTree.AddKeyValue(15, 15);
+            // Breadth-first traversal test 
+            Console.WriteLine("Breadth - first traversal test");
+            if (BinTree.WideAllNodes().Count == 15 && BinTree.WideAllNodes()[0].NodeKey == 8 && BinTree.WideAllNodes()[1].NodeKey == 4
+                && BinTree.WideAllNodes()[2].NodeKey == 12 && BinTree.WideAllNodes()[3].NodeKey == 2 && BinTree.WideAllNodes()[4].NodeKey == 6
+                && BinTree.WideAllNodes()[5].NodeKey == 10 && BinTree.WideAllNodes()[6].NodeKey == 14 && BinTree.WideAllNodes()[7].NodeKey == 1
+                && BinTree.WideAllNodes()[8].NodeKey == 3 && BinTree.WideAllNodes()[9].NodeKey == 5 && BinTree.WideAllNodes()[10].NodeKey == 7
+                && BinTree.WideAllNodes()[11].NodeKey == 9 && BinTree.WideAllNodes()[12].NodeKey == 11 && BinTree.WideAllNodes()[13].NodeKey == 13
+                && BinTree.WideAllNodes()[14].NodeKey == 15)
+            {
+                Console.WriteLine("OK");
+            }
+            else
+            {
+                Console.WriteLine("FAIL");
+            }
+            // Depth-first traversal tests
+            // 0 (in-order)
+            Console.WriteLine("Depth-first in-order traversal test");
+            if (BinTree.DeepAllNodes(0).Count == 15 && BinTree.DeepAllNodes(0)[0].NodeKey == 1 && BinTree.DeepAllNodes(0)[1].NodeKey == 2
+                && BinTree.DeepAllNodes(0)[2].NodeKey == 3 && BinTree.DeepAllNodes(0)[3].NodeKey == 4 && BinTree.DeepAllNodes(0)[4].NodeKey == 5
+                && BinTree.DeepAllNodes(0)[5].NodeKey == 6 && BinTree.DeepAllNodes(0)[6].NodeKey == 7 && BinTree.DeepAllNodes(0)[7].NodeKey == 8
+                && BinTree.DeepAllNodes(0)[8].NodeKey == 9 && BinTree.DeepAllNodes(0)[9].NodeKey == 10 && BinTree.DeepAllNodes(0)[10].NodeKey == 11
+                && BinTree.DeepAllNodes(0)[11].NodeKey == 12 && BinTree.DeepAllNodes(0)[12].NodeKey == 13 && BinTree.DeepAllNodes(0)[13].NodeKey == 14
+                && BinTree.DeepAllNodes(0)[14].NodeKey == 15)
+            {
+                Console.WriteLine("OK");
+            }
+            else
+            {
+                Console.WriteLine("FAIL");
+            }
+            // 1 (post-order)
+            Console.WriteLine("Depth-first post-order traversal test");
+            if (BinTree.DeepAllNodes(1).Count == 15 && BinTree.DeepAllNodes(1)[0].NodeKey == 1 && BinTree.DeepAllNodes(1)[1].NodeKey == 3
+                && BinTree.DeepAllNodes(1)[2].NodeKey == 2 && BinTree.DeepAllNodes(1)[3].NodeKey == 5 && BinTree.DeepAllNodes(1)[4].NodeKey == 7
+                && BinTree.DeepAllNodes(1)[5].NodeKey == 6 && BinTree.DeepAllNodes(1)[6].NodeKey == 4 && BinTree.DeepAllNodes(1)[7].NodeKey == 9
+                && BinTree.DeepAllNodes(1)[8].NodeKey == 11 && BinTree.DeepAllNodes(1)[9].NodeKey == 10 && BinTree.DeepAllNodes(1)[10].NodeKey == 13
+                && BinTree.DeepAllNodes(1)[11].NodeKey == 15 && BinTree.DeepAllNodes(1)[12].NodeKey == 14 && BinTree.DeepAllNodes(1)[13].NodeKey == 12
+                && BinTree.DeepAllNodes(1)[14].NodeKey == 8)
+            {
+                Console.WriteLine("OK");
+            }
+            else
+            {
+                Console.WriteLine("FAIL");
+            }
+            // 2 (pre-order)
+            Console.WriteLine("Depth-first pre-order traversal test");
+            if (BinTree.DeepAllNodes(2).Count == 15 && BinTree.DeepAllNodes(2)[0].NodeKey == 8 && BinTree.DeepAllNodes(2)[1].NodeKey == 4
+                && BinTree.DeepAllNodes(2)[2].NodeKey == 2 && BinTree.DeepAllNodes(2)[3].NodeKey == 1 && BinTree.DeepAllNodes(2)[4].NodeKey == 3
+                && BinTree.DeepAllNodes(2)[5].NodeKey == 6 && BinTree.DeepAllNodes(2)[6].NodeKey == 5 && BinTree.DeepAllNodes(2)[7].NodeKey == 7
+                && BinTree.DeepAllNodes(2)[8].NodeKey == 12 && BinTree.DeepAllNodes(2)[9].NodeKey == 10 && BinTree.DeepAllNodes(2)[10].NodeKey == 9
+                && BinTree.DeepAllNodes(2)[11].NodeKey == 11 && BinTree.DeepAllNodes(2)[12].NodeKey == 14 && BinTree.DeepAllNodes(2)[13].NodeKey == 13
+                && BinTree.DeepAllNodes(2)[14].NodeKey == 15)
+            {
+                Console.WriteLine("OK");
+            }
+            else
+            {
+                Console.WriteLine("FAIL");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
