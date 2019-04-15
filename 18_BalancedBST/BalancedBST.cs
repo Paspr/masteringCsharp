6a08088b9faf89c23e702e4670ca7c760b6093d6
Pavel Osminin <pav.osminin@gmail.com>
2019-04-15 23:01:51 +0500

    obscure version

---
+++ 18_BalancedBST/BalancedBST.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsDataStructures2
+{
+    public static class BalancedBST
+    {
+        public static int[] GenerateBBSTArray(int[] a)
+        {
+            Array.Sort(a);
+
+            int min = 0;
+            int max = a.Length - 1;
+            int[] BSTarray = new int[a.Length];
+            BuildArray(a, min, max);
+
+            int BuildArray (int[] num, int minInput, int maxInput)
+            {
+                int middle = (minInput + maxInput) / 2;
+                num[0] = a[middle];
+                if (minInput > maxInput)
+                {
+                    return 0;
+                }
+                else
+                {
+                    return (BuildArray(num, minInput, middle - 1));
+                    BuildArray(num, middle + 1, maxInput);
+                }
+                //return 0;
+            }
+
+
+            return BSTarray;
+        }
+    }
+}
