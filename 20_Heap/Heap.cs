e8679c42f66c274765a8f18f7ae6257cfc1316e8
Pavel Osminin <pav.osminin@gmail.com>
2019-05-12 00:15:13 +0500

    initial version

---
+++ 20_Heap/Heap.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsDataStructures2
+{
+    public class Heap
+    {
+
+        public int[] HeapArray; // хранит неотрицательные числа-ключи
+
+        public Heap() { HeapArray = null; }
+
+        public void MakeHeap(int[] a, int depth)
+        {
+            // создаём массив кучи HeapArray из заданного
+            // размер массива выбираем на основе глубины depth
+            // ...
+            HeapArray = new int[(int)Math.Pow(2, depth + 1) - 1];
+            for (int i=0; i<a.Length; i++)
+            {
+                Add(a[i]);
+            }
+        }
+
+        public int GetMax()
+        {
+            // вернуть значение корня и перестроить кучу
+            if (HeapArray[0]==0)
+            {
+                return -1; // если куча пуста
+            }
+            else
+            {
+                int max = HeapArray[0];
+                HeapArray[0] = HeapArray[GetIndexNotNull(HeapArray)];
+                HeapArray[GetIndexNotNull(HeapArray)] = 0;
+                SiftDown(0);
+                return max;
+            }
+        }
+
+        public bool Add(int key)
+        {
+            // добавляем новый элемент key в кучу и перестраиваем её
+            if (HeapArray[0] == 0)
+            {
+                HeapArray[0] = key;
+                return true;
+            }
+
+            int index = GetIndexNull(HeapArray);
+            if (index != -1)
+            {
+                HeapArray[index] = key;
+                SiftUp(index);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void SiftUp(int index)
+        {
+            int temp;
+            while (HeapArray[index] > HeapArray[(index - 1) / 2])
+            {
+                temp = HeapArray[(index - 1) / 2];
+                HeapArray[(index - 1) / 2] = HeapArray[index];
+                HeapArray[index] = temp;
+                index = (index - 1) / 2;
+            }
+        }
+    
+        public void SiftDown(int index)
+        {
+            int temp;
+            while (2 * index + 1 < GetElementsNumber(HeapArray))
+            {     
+                int left = 2 * index + 1;
+                int right = 2 * index + 2;
+                int j = left;
+                if (HeapArray[right] > HeapArray[left])
+                {
+                    j = right;
+                }
+                if (HeapArray[index] >= HeapArray[j])
+                {
+                    break;
+                }
+                else
+                {
+                    temp = HeapArray[index];
+                    HeapArray[index] = HeapArray[j];
+                    HeapArray[j] = temp;
+                }
+                index = j;
+            }
+        }
+
+        public int GetIndexNull(int[] a)
+        {
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] == 0) return i;
+                
+            }
+            return -1;
+        }
+
+        public int GetElementsNumber(int[] a)
+        {
+            int j = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] != 0) j++;
+
+            }
+            return j;
+        }
+
+        public int GetIndexNotNull(int[] a)
+        {
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] == 0) return i-1;
+
+            }
+            return a.Length-1;
+        }
+    }
+}
---
+++ 20_Heap/Tests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsDataStructures2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] testA = new int[] {1, 2, 4, 5, 6, 8, 9, 10, 11, 16 };
+            Heap test = new Heap();
+            test.MakeHeap(testA, 3);
+            Console.WriteLine("Making heap test");
+            if (test.HeapArray[0] == 16 && test.HeapArray[1] == 11 && test.HeapArray[2] == 8 &&
+                test.HeapArray[3] == 9 && test.HeapArray[4] == 10 && test.HeapArray[5] == 2 &&
+                test.HeapArray[6] == 6 && test.HeapArray[7] == 1 && test.HeapArray[8] == 5 &&
+                test.HeapArray[9] == 4)
+            {
+                Console.WriteLine("OK");
+            }
+            else
+            {
+                Console.WriteLine("FAIL");
+            }
+            Console.WriteLine("Get max element from a full heap");
+            if (test.GetMax()==16 && test.HeapArray[0] == 11 && test.HeapArray[1] == 10 &&
+                test.HeapArray[2] == 8 && test.HeapArray[3] == 9 && test.HeapArray[4] == 4 &&
+                test.HeapArray[5] == 2 && test.HeapArray[6] == 6 && test.HeapArray[7] == 1 &&
+                test.HeapArray[8] == 5 && test.HeapArray[9] == 0)
+            {
+                Console.WriteLine("OK");
+            }
+            else
+            {
+                Console.WriteLine("FAIL");
+            }
+            for (int i=0; i<10; i++)
+            {
+                test.GetMax();
+            }
+            Console.WriteLine("Get max element from an empty heap");
+            if (test.GetMax() == -1 && test.HeapArray[0] == 0)
+            {
+                Console.WriteLine("OK");
+            }
+            else
+            {
+                Console.WriteLine("FAIL");
+            }
+        }
+    }
+}
